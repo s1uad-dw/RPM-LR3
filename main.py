c64eb38639931dc7648791006f79e7011dddb413
s1uad-dw <s1uad.dw@gmail.com>
2022-01-28 22:47:56 +0300

    collatz ready

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ def x3_1(x):
 
 def x2(x):
     return x // 2
+
+def collatz():
+    x = input_function()
+    if x%2==0:
+        print('x - чётное\nx/2 =', x2(x))
+    else:
+        print('x - нечётное\nx*3+1 =', x3_1(x))
+
+collatz()
